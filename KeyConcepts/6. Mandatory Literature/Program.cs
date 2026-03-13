d3786b347980c4a22d4dbe8c390ab5172126f9d9
BBCooL40 <anton.tzonev@yahoo.com>
2026-03-13 12:35:02 +0200

    Add latest tasks and new project

---
+++ KeyConcepts/6. Mandatory Literature/Program.cs
@@ -0,0 +1,5 @@
+﻿int pagesInTheBook = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int daysToFinish = int.Parse(Console.ReadLine());
+int hoursToReadPerDay = (pagesInTheBook / pagesPerHour) / daysToFinish;
+Console.WriteLine(hoursToReadPerDay);
---
+++ KeyConcepts/7. Supplies for School/Program.cs
@@ -0,0 +1,17 @@
+﻿
+double pensPrice = 5.80;
+double markersPrice = 7.20;
+double boardCleanerPricePerLiter = 1.20;
+
+int numberOfPensPackages = int.Parse(Console.ReadLine());
+int numberOfMarkersPackages = int.Parse(Console.ReadLine());
+int LitersOfBoardCleaner = int.Parse(Console.ReadLine());
+int discount =  int.Parse(Console.ReadLine());
+double discountPercentage = discount / 100.0;
+
+double costOfPens = pensPrice * numberOfPensPackages;
+double costOfMarkers = markersPrice * numberOfMarkersPackages;
+double costOfBoardCleaner = boardCleanerPricePerLiter * LitersOfBoardCleaner;
+double costOfMaterials = costOfPens +  costOfMarkers + costOfBoardCleaner;
+double finalCost = costOfMaterials * (1 - discountPercentage);
+Console.WriteLine(finalCost);
